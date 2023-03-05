4f479a81583bc999b4b192354f3e670f3f83b48a
jbc103 <jbc@psu.edu>
2023-03-05 16:22:07 -0500

    Horseshow Amateur Radio Club Site New

---
+++ footer.js
@@ -0,0 +1,3 @@
+document.write('<footer class="container-fluid text-center" style="font-size:x-small">')
+document.write('Copyright ©2023 Horseshoe Amateur Radio Club of Altoona/Blair County, PA. All Rights Reserved.')
+document.write('</footer>')
